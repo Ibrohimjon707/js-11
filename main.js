7cf2cef2b45a7be3035b0551987164a74a90be9f
Ibrohimjon707 <ibrohimjonathamjonov38@gmail.com>
2025-01-27 17:39:07 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+let ege = +prompt('age kirit')
+let resume = 2025 - ege
+alert(`Siz ${resume} yilsiz`)
+let uzunligi = prompt("string metodida malumot kriting")
+
+if(uzunligi.length < 10){
+    alert("o'rtacha")
+}
+else if(uzunligi.length > 10){
+    alert("Uzun")
+}
+
+
+
+//    'aeiouo'
+    let ibrohim = prompt("unli va undosh harfda gap kriting");
+ if(ibrohim === 'a'){
+    alert("bu unli harflar")
+}
+else if(ibrohim === 'e'){
+    alert("bu unli harflar")
+}
+else if(ibrohim === 'i'){
+    alert("bu unli harflar")
+}
+else if(ibrohim === 'o'){
+    alert("bu unli harflar")
+}
+else if(ibrohim === 'u'){
+    alert("bu unli harflar")
+}
+else if(ibrohim === 'o'){
+    alert("bu unli harflar")
+}
+else{
+    alert("Bu undosh harflar")
+}
+
+
+
+
